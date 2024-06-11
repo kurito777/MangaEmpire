9aefaea5ea8f85c2262c4a49bbfdcfe5054c9ae4
Patito09 <pa.salinasm@duocuc.cl>
2024-06-10 20:14:23 -0400

    migraciones de modelos a db

--- backend1/mangaentorno/mangaproject1/startapp/models.py
+++ backend1/mangaentorno/mangaproject1/startapp/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-from django.contrib.auth import User
+from django.contrib.auth.models import User  # Importación corregida
 
 # Create your models here.
-
 
 class Author(models.Model):
     name = models.CharField(max_length=60)
@@ -11,6 +10,7 @@ class Author(models.Model):
 
     def __str__(self):
         return f"{self.name} {self.last_name}"
+
 class Manga(models.Model):
     idManga = models.CharField(max_length=13, primary_key=True, unique=True, null=False)
     title = models.CharField(max_length=100)
@@ -18,20 +18,21 @@ class Manga(models.Model):
     estado = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.title} {self.estado} {Author}"
-    
+        return f"{self.title} {self.estado} {self.author.name} {self.author.last_name}"
+
 class TipoSubscripcion(models.Model):
     nombre = models.CharField(max_length=60)
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return self.nombre   
+        return self.nombre
+
 class Subscripcion(models.Model):
     tipo = models.ForeignKey(TipoSubscripcion, on_delete=models.CASCADE)
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)  # Usando la importación correcta
     fecha_inicio = models.DateField()
     fecha_fin = models.DateField()
 
     def __str__(self):
-        return f"{self.usuario.user} - {self.tipo.nombre}"
+        return f"{self.usuario.username} - {self.tipo.nombre}"  # Corrección en la referencia a `username`
